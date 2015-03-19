f7171a0d5fcfcd470f29223e54430d55219829b6
CJ Carey <perimosocordiae@gmail.com>
2015-03-19 02:21:13 -0400

    Fixing in-place ops test for truediv

--- pytests/math/inplaceTest.py
+++ pytests/math/inplaceTest.py
@@ -1,3 +1,4 @@
+from __future__ import division
 
 def inplace_ops(x, integral=True):
   x += 1
@@ -6,13 +7,9 @@ def inplace_ops(x, integral=True):
   print x
   x *= 2
   print x
-  x /= 2
-  print x
-  x **= 2
-  print x
   x //= 2
   print x
-  x %= 10
+  x **= 2
   print x
   if integral:
     x >>= 2
@@ -25,8 +22,12 @@ def inplace_ops(x, integral=True):
     print x
     x ^= 4
     print x
+  x /= 2
+  print x
+  x %= 10
+  print x
 
 inplace_ops(7)
 inplace_ops(7L)
 inplace_ops(7., integral=False)
-inplace_ops(7j, integral=False)
+inplace_ops(7+7j, integral=False)
