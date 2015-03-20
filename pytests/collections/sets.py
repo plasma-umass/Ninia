fc5bfe3df3723a320d3ffab6396cbc55430f4f88
CJ Carey <perimosocordiae@gmail.com>
2015-03-20 10:50:47 -0400

    More comparable test for sets

--- pytests/collections/sets.py
+++ pytests/collections/sets.py
@@ -3,9 +3,15 @@ a = set(['a', 'b', 'c', 'd'])
 b = set(['c', 'd', 'e', 'f'])
 c = set(['a', 'c'])
 
-print a & b
+def canonical_print(x):
+  print repr(x)[:3], sorted(x)
+
+
+canonical_print(a & b)
+canonical_print(a - b)
+canonical_print(a ^ b)
+canonical_print(a | b)
+
+# subset checks
 print c < a
-print a - b
-print a ^ b
-print a | b
 
