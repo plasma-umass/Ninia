04bddf163bdb202415615f62e00e482d3c958c51
John Vilk <jvilk@cs.umass.edu>
2015-06-29 00:52:02 -0400

    Fixing test runner.

--- console/test.ts
+++ console/test.ts
@@ -14,6 +14,13 @@ function realPrint(text: string) {
     oldStdout.apply(process.stdout, [text]);
 }
 
+function restorePrint(cb: () => void): void {
+    var newStdout = process.stdout.write;
+    process.stdout.write = oldStdout;
+    cb();
+    process.stdout.write = newStdout;
+}
+
 function test(name: string, file: string) {
     realPrint(`Running ${name}... `);
     numTests += 1;
@@ -38,37 +45,41 @@ function test(name: string, file: string) {
         numPassed += 1;
     } else {
         realPrint("Fail\n");
-        console.log('CPython output:\n', expectedOut);
-        console.log('Ninia output:\n', testOut);
-        if (err) {
-            console.log(err);
-        }
+        restorePrint(() => {
+            console.log('CPython output:\n', expectedOut);
+            console.log('Ninia output:\n', testOut);
+            if (err) {
+                console.log(err);
+            }
+        });
         testFails[testName] += 1;
     }
 }
 
 function printResults() {
-    console.log(`\n--- Results ---`);
-    for (var tName in testFails) {
-        if (testFails[tName] > 0) {
-            console.log(`${testFails[tName]} test failed in ${tName} tests.`);
+    restorePrint(() => {
+       console.log(`\n--- Results ---`);
+        for (var tName in testFails) {
+            if (testFails[tName] > 0) {
+                console.log(`${testFails[tName]} test failed in ${tName} tests.`);
+            }
         }
-    }
-    console.log(`Passed ${numPassed}/${numTests} tests.`);
+        console.log(`Passed ${numPassed}/${numTests} tests.`); 
+    });
 }
 
 // Add more tests here:
-console.log(`\n--- Math tests ---`);
+realPrint(`\n--- Math tests ---`);
 test("Unary operations test", "pytests/math/unaryOpsTest");
 test("Binary operations test", "pytests/math/binaryOpsTest");
 test("In-place operations test", "pytests/math/inplaceTest");
 test("Mixed Arithmetic test", "pytests/math/mixedMathTest");
-console.log(`\n--- Function tests ---`);
+realPrint(`\n--- Function tests ---`);
 test("Keyword and default arguments test","pytests/functions/keywordargs");
 test("Recursion test", "pytests/functions/recursionTest");
 test("Scoping test", "pytests/functions/scopeTest");
 test("Generators test", "pytests/functions/generatorTest");
-console.log(`\n--- Builtin tests ---`);
+realPrint(`\n--- Builtin tests ---`);
 test("Builtin Types test", "pytests/builtins/builtinTypes");
 test("Bin function test", "pytests/builtins/bin");
 test("Hex function test", "pytests/builtins/hex");
@@ -81,17 +92,17 @@ test("All & Any functions test", "pytests/builtins/all_any");
 test("Chr & Ord functions test", "pytests/builtins/chr_ord");
 test("Attribute accessors test", "pytests/builtins/attrs");
 test("Underscore names test", "pytests/builtins/underscoresTest");
-console.log(`\n--- Collection tests ---`);
+realPrint(`\n--- Collection tests ---`);
 test("List test", "pytests/collections/lists");
 test("Set test", "pytests/collections/sets");
 test("Dict test", "pytests/collections/dicts");
-console.log(`\n--- Control flow tests ---`);
+realPrint(`\n--- Control flow tests ---`);
 test("Loop test", "pytests/loopTest");
 test("Range test", "pytests/rangeTest");
 test("Comprehension test", "pytests/comprehensionTest");
-console.log(`\n--- Class tests ---`);
+realPrint(`\n--- Class tests ---`);
 test("Basic class test", "pytests/classes/userDefTest");
-console.log(`\n--- Other tests ---`);
+realPrint(`\n--- Other tests ---`);
 test("Strings test", "pytests/stringTest");
 test("Slice test", "pytests/sliceTest");
 test("Assignment test", "pytests/assignmentTest");
