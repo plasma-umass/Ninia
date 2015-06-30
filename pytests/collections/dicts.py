b49bfe780a920aeee7a1d405d041a0b4810aa9c2
John Vilk <jvilk@cs.umass.edu>
2015-06-29 21:38:06 -0700

    Wrapping / exposing more functions at the Python level.

--- pytests/collections/dicts.py
+++ pytests/collections/dicts.py
@@ -8,3 +8,9 @@ def dict_test(d):
 dict_test(dict(foo=4, bar='x'))
 dict_test({'foo': 4, 'bar': 'x', 5: 3.0})
 
+l = dict(f=4)
+print l.__getitem__('f')
+l.__setitem__('f', 4)
+print l['f']
+l.__delitem__('f')
+print l
